e3cee721006f6a7a6c23a9405ffbb9ca28fcc0d4
cpppy <1107706059@qq.com>
2024-11-14 22:38:45 +0800

    update

--- generate/eval_private_data_v4_mcq.py
+++ generate/eval_private_data_v4_mcq.py
@@ -129,6 +129,44 @@ class AnswerBot:
         response_str = response_str[0:8].strip()
         return response_str
 
+    def inference_completion(self, messages):
+        # from llama_index.core.llms import ChatMessage, MessageRole, ChatResponse
+        # _messages = []
+        # for m in messages:
+        #     if m['role'] == 'system':
+        #         _messages.append(ChatMessage(role=MessageRole.SYSTEM, content=m['content']))
+        #     elif m['role'] == 'user':
+        #         _messages.append(ChatMessage(role=MessageRole.USER, content=m['content']))
+        #     if m['role'] == 'assistant':
+        #         _messages.append(ChatMessage(role=MessageRole.ASSISTANT, content=m['content']))
+        #
+        # # response = self.llm.app(_messages)
+        # # response_str = response.message.content
+        # input_ids = self.tokenizer.apply_chat_template(
+        #     messages,
+        #     add_generation_prompt=True,
+        #     return_tensors="pt",
+        # ).cuda()
+        input_ids = self.tokenizer.encode(messages[0]['content'])
+
+        outputs = self.model.generate(
+            inputs=input_ids,
+            max_new_tokens=256,
+            do_sample=True,
+            temperature=0.1,
+            top_k=3,
+            top_p=0.6,
+            pad_token_id=self.tokenizer.eos_token_id
+        )
+
+        response = outputs[0][input_ids.shape[-1]:]
+        response_str = self.tokenizer.decode(response, skip_special_tokens=True)
+        # exit(0)
+
+        # print(response_str)
+        response_str = response_str[0:8].strip()
+        return response_str
+
 
 # def to_llama_format(message: list):
 #     system_message, *other_messages, user_message = message
